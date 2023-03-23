ec72d8c9fcc4e107128a8b269c8dd1e723282f97
tasiby <tuanhoang.uit@gmail.com>
2023-03-23 13:40:42 +0700

    feat: basic nft factory

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,19 +2,12 @@
 pragma solidity 0.8.19;
 
 import { INFT } from "./interfaces/INFT.sol";
-
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
 import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-
 import { ERC721EnumerableUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-
 import { ERC721URIStorageUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
 contract NFT is
--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -5,4 +5,30 @@ import { NFT } from "./NFT.sol";
 import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTFactory is Ownable {}
+contract NFTFactory is Ownable {
+    NFT[] public nfts;
+    address implement;
+
+    constructor(address implement_) {
+        implement = implement_;
+    }
+
+    function setImplement(address newImplement_) external onlyOwner {
+        implement = newImplement_;
+    }
+
+    function clone(
+        address marketplace_,
+        string calldata name_,
+        string calldata symbol_,
+        string calldata baseURI_
+    ) external {
+        NFT newNft = NFT(Clones.clone(implement));
+        newNft.initialize(marketplace_, name_, symbol_, baseURI_);
+        nfts.push(newNft);
+    }
+
+    function getClone() external view returns (NFT[] memory) {
+        return nfts;
+    }
+}
